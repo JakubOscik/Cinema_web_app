000f2380c2f8037b547ec78cd3bfcc9be99256cd
Zvinn <77742224+Zvinn@users.noreply.github.com>
2022-05-24 19:52:44 +0200

    Login Endpoint

--- src/main/java/com/movietime/MovieTime/controller/UsersController.java
+++ src/main/java/com/movietime/MovieTime/controller/UsersController.java
@@ -1,6 +1,7 @@
 package com.movietime.MovieTime.controller;
 
 import com.movietime.MovieTime.entity.Users;
+import com.movietime.MovieTime.repository.UsersRepository;
 import com.movietime.MovieTime.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,11 @@ public class UsersController
 {
     @Autowired
     UsersService usersService;
+    private final UsersRepository usersRepository;
+    @Autowired
+    public UsersController(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
 
     @GetMapping("/users")
     private List<Users> getAllUsers()
@@ -37,5 +43,30 @@ public class UsersController
     {
         usersService.saveOrUpdate(users);
         return users.getUserId();
+    }
+
+//    @PostMapping("/login")
+//    private boolean login(@RequestParam String username, @RequestParam String password){
+//        Iterable<Users> allUsers = usersRepository.findAll();
+//        boolean status = false;
+//        for(Users user: allUsers) {
+//            if(user.getEmail().equals(username) && user.getPassword().equals(password)){
+//                status = true;
+//            }
+//        }
+//        System.out.println(status + "AAAAAAAAAAAAAAAAAAAAAAAAAA");
+//        return status;
+//    }
+
+    @PostMapping("/login")
+    private boolean login(@RequestBody Users data){
+        Iterable<Users> allUsers = usersRepository.findAll();
+        boolean status = false;
+        for(Users user: allUsers) {
+            if(user.getEmail().equals(data.getEmail()) && user.getPassword().equals(data.getPassword())){
+                status = true;
+            }
+        }
+        return status;
     }
 }
