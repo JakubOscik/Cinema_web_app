16b2c9e3ff5cd0e1fa8d0baeae8495303329f5aa
Zvinn <77742224+Zvinn@users.noreply.github.com>
2022-05-31 19:34:38 +0200

    Add files via upload

---
+++ src/main/java/com/movietime/MovieTime/controller/EmailController.java
@@ -0,0 +1,20 @@
+package com.movietime.MovieTime.controller;
+
+import com.movietime.MovieTime.service.EmailService;
+import com.movietime.MovieTime.service.PlaceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    EmailService emailService;
+
+    @RequestMapping(value = "/sendemail")
+    public void sendEmail() {
+        emailService.sendEmail();
+    }
+}
