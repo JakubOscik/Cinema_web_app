13f858aa2cb8f249e3c694ee2233c4f20aa7d487
MagdalenaPres <80152110+MagdalenaPres@users.noreply.github.com>
2022-06-01 00:42:01 +0200

    comment email

--- src/main/java/com/movietime/MovieTime/service/EmailService.java
+++ src/main/java/com/movietime/MovieTime/service/EmailService.java
@@ -1,25 +1,26 @@
 package com.movietime.MovieTime.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmailService {
-
-    @Autowired
-    private JavaMailSender mailSender;
-
-    public void sendEmail() {
-        String from = "sender@gmail.com";
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setFrom(from);
-        message.setTo("oscikj@gmail.com");
-        message.setSubject("MovieTime");
-        message.setText("Hello! Your ticket is here.");
-
-        mailSender.send(message);
-    }
-}
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class EmailService {
+//
+//    @Autowired
+//    private JavaMailSender mailSender;
+//
+//    public void sendEmail() {
+//        String from = "sender@gmail.com";
+//        SimpleMailMessage message = new SimpleMailMessage();
+//
+//        message.setFrom(from);
+//        message.setTo("oscikj@gmail.com");
+//        message.setSubject("MovieTime");
+//        message.setText("Hello! Your ticket is here.");
+//
+//        mailSender.send(message);
+//    }
+//}
+//
