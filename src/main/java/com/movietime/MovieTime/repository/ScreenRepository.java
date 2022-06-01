5f33684cdc438ae352cd2244f7647a8a340f1383
Zvinn <oscikj@gmail.com>
2022-06-01 10:22:31 +0200

    Update screenRepo to get ids

--- src/main/java/com/movietime/MovieTime/repository/ScreenRepository.java
+++ src/main/java/com/movietime/MovieTime/repository/ScreenRepository.java
@@ -13,6 +13,6 @@ public interface ScreenRepository extends CrudRepository<Screen, Integer>
 //    @Query("SELECT s.SCREEN_DAY, s.SCREEN_HOUR, f.NAME FROM Screen s INNER JOIN Film f ON s.FILM_ID = f.FILM_ID GROUP BY f.NAME " +
 //            "WHERE s.SCREEN_DAY like %?1")
 //    List<Screen> findAllFilmsByDay(String day);
-    @Query("SELECT s.screenDay, s.screenHour, s.filmId FROM Screen s WHERE s.screenDay=:day")
+    @Query("SELECT s.screenDay, s.screenHour, s.filmId, s.screenId FROM Screen s WHERE s.screenDay=:day")
     List<Map<String, Object>> findAllFilmsByDay(@Param("day") String day);
 }
--- src/main/java/com/movietime/MovieTime/service/ScreenService.java
+++ src/main/java/com/movietime/MovieTime/service/ScreenService.java
@@ -39,29 +39,38 @@ public class ScreenService
 
     public JSONArray printScreen(String day) {
         Map<Object,ArrayList<Object>> filmsData = new HashMap<>();
+        Map<Object,ArrayList<Object>> screenIds = new HashMap<>();
         var s = screenRepository.findAllFilmsByDay(day);
         for (Map<String, Object> stringObjectMap : s) {
-            var single = stringObjectMap.values().stream().toList();
+            List<Object> single = stringObjectMap.values().stream().toList();
             Object name = single.get(2);
             Object hour = single.get(1);
+            Object id = single.get(3);
             if (!filmsData.containsKey(name)) {
                 ArrayList<Object> hours = new ArrayList<>();
+                ArrayList<Object> ids = new ArrayList<>();
                 hours.add(hour);
+                ids.add(id);
                 filmsData.put(name, hours);
+                screenIds.put(name, ids);
             } else {
                 filmsData.get(name).add(hour);
-                filmsData.put(name,  filmsData.get(name));
+                filmsData.put(name, filmsData.get(name));
+                screenIds.get(name).add(id);
+                screenIds.put(name, screenIds.get(name));
             }
 
         }
         JSONArray array = new JSONArray();
-
-        for(var a: filmsData.keySet()) {
-            var name = a;
-            var hours = filmsData.get(a);
+        int i = 0;
+        for(Object a: filmsData.keySet()) {
+            Object name = a;
+            ArrayList<Object> hours = filmsData.get(a);
+            ArrayList<Object> ids = screenIds.get(a);
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("data", name);
             jsonObject.put("hours", hours);
+            jsonObject.put("id", ids);
             array.add(jsonObject);
         }
         return array;
